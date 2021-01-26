47da642fe4a870133fe314de665e0e799e751585
Lukaczynski <lukimovil@gmail.com>
2021-01-26 22:59:13 +0100

    Basic interruptions test

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -7,34 +7,109 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include <inttypes.h>
 
+/**
+ * Rotatory encoder parameters
+ */
+
+#define GPIO_INPUT_IO_0 32 // CLK
+#define GPIO_INPUT_IO_1 33 // DT
+#define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_INPUT_IO_0) | (1ULL << GPIO_INPUT_IO_1))
+#define ESP_INTR_FLAG_DEFAULT 0
+static xQueueHandle gpio_evt_queue = NULL;
+
+
+volatile uint64_t threshold = 10000;
+volatile int64_t rotaryHalfSteps = 0;
+
+volatile uint64_t int0_time = 0;
+volatile uint64_t int1_time = 0;
+volatile int8_t int0_signal = 0;
+volatile uint8_t int1_signal = 0;
+volatile uint8_t int0_history = 0;
+volatile uint8_t int1_history = 0;
+
+/** 
+ */
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    uint32_t gpio_num = (uint32_t)arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void *arg)
+{
+    uint32_t io_num;
+    for (;;)
+    {
+        if (xQueueReceive(gpio_evt_queue, &io_num, 10000))
+        {
+            rotaryHalfSteps++;
+            //printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    ///io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    //io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    //io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    //io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    //gpio_config(&io_conf);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
 
-    printf("silicon revision %d, ", chip_info.revision);
+    //change gpio intrrupt type for one pin
+    //gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *)GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    //gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    //gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+    // int cnt = 0;
+    while(1) {
+        printf("cnt: %" PRIu64 "\n", rotaryHalfSteps);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        // gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+        // gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
